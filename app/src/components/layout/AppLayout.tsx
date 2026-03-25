b468e8812cc1729d5467fbfb7db8f5e5b0e94283
Matheus <mathe@teucontador.com.br>
2026-03-25 15:22:01 -0300

    fix: logo sidebar 200px

--- app/src/components/layout/AppLayout.tsx
+++ app/src/components/layout/AppLayout.tsx
@@ -462,7 +462,7 @@ export function AppLayout() {
 
       <Sidebar $open={sidebarOpen}>
         <LogoWrap>
-          <img src="/img/logo.png" alt="TEUcontador" style={{ height: 90, width: 'auto', maxWidth: '100%', display: 'block' }} />
+          <img src="/img/logo.png" alt="TEUcontador" style={{ height: 200, width: 'auto', maxWidth: '100%', display: 'block' }} />
         </LogoWrap>
 
         <NavSection>
